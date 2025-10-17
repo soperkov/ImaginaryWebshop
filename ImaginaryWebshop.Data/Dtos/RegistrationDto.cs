44d089de65d2690e61928a32f6683bf7cc5b9afb
soperkov <soperkov@vsite.hr>
2025-10-17 18:54:15 +0200

    added registration dto with annotations

---
+++ ImaginaryWebshop.Data/Dtos/RegistrationDto.cs
@@ -0,0 +1,19 @@
+﻿namespace ImaginaryWebshop.Data.Dtos
+{
+    public class RegistrationDto
+    {
+        [Required(ErrorMessage = "Username is required!")]
+        public string? Username { get; set; }
+        [Required(ErrorMessage = "Email is required!")]
+        [EmailAddress(ErrorMessage = "Email must be valid.")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "You must type in your password.")]
+        [MinLength(8, ErrorMessage = "Your password must contain at least 8 characters")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$",
+            ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, and one digit.")]
+        public string? Password { get; set; }
+        [Required(ErrorMessage = "This field must not be empty.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
