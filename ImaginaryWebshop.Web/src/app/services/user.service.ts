cdf9d5fd814c889506ccbed5ec6314dc808ad6e0
soperkov <soperkov@vsite.hr>
2025-10-18 21:51:31 +0200

    added user service methods

--- ImaginaryWebshop.Web/src/app/services/user.service.ts
+++ ImaginaryWebshop.Web/src/app/services/user.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { RegistrationDto, UserDetailsDto, UserLoginDto } from '../models/user.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  
+  private base = `${environment.apiBaseUrl}/user`;
+
+  constructor(private http:HttpClient) {}
+
+  register(user: RegistrationDto): Observable<string> {
+    return this.http.post<string>(`${this.base}/register`, user);
+  }
+
+  login(user: UserLoginDto): Observable<string> {
+    return this.http.post<string>(`${this.base}/login`, user);
+  }
+
+  getDetails(userId: string): Observable<UserDetailsDto> {
+    return this.http.get<UserDetailsDto>(`${this.base}/${userId}`);
+  }
 }
